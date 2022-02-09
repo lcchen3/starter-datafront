5ce21c535f59b8217ab9db143f92f96eb673c2d2
lcchen3 <lcchen3@mit.edu>
2022-02-09 13:11:27 -0500

    lec 3 updates

--- working_with_data/05_callbacks/01_filter.js
+++ working_with_data/05_callbacks/01_filter.js
@@ -1,11 +1,18 @@
 var list = [5,8,9,2,7,6,3,1,4];
 
+// could pass this function instead of "callback" parameter in filter
 function callback(list){
-    // to be done
+    // to be done, user provides function that acts on list
+    var newlist = [];
+    for(var i=0;i<list.length; i++){
+        if(list[i]>5) newlist.push(list[i]); //put into array
+    }
+    return newlist;
 }
 
 function filter(list, callback){
-    // to be done
+    return callback(list); 
 }
  
 var filtered = filter(list, callback);
+console.log(filtered);
--- working_with_data/05_callbacks/02_filter.js
+++ working_with_data/05_callbacks/02_filter.js
@@ -2,6 +2,10 @@ var list = [5,8,9,2,7,6,3,1,4];
 
 function callback(item){
     // return true for included items
+    return item > 5;
 }
 
-var filtered = list.filter(callback);
+var filtered = list.filter(callback); //filter returns ID of elements for true
+console.log(filtered);
+
+//call this script with loader.html - in same directory
